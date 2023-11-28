211bf6c3fb01f80281df22f6f373927e7b1fb541
AntoninLT <laborde-@insa-toulouse.fr>
2023-11-28 19:56:59 +0100

    reverse closed loop & attenuation first order filter

--- raspberryPI3/ros2_ws/src/car_control/include/car_control/control_loop.h
+++ raspberryPI3/ros2_ws/src/car_control/include/car_control/control_loop.h
@@ -6,19 +6,18 @@
 
 #define Te 0.1  // Motors data (right/left feedback) are periodically send with a period egals to 100ms.
 #define K 4.59  // Ki/Kp 
+#define To 0.333// Time constant of first order filter
 
 static double ao;
 static double bo;
+static double eo;
+static double fo;
+// Calculate the recurrence equation based on the first order attenuation filter to avoid the wheels losing grip
+void attenuation_recurrence(double& PWM_order,double PWM_order_last,double& PWM_att_last);
 
-/* Calculate the recurrence equation based on the compensator to move the car forward and backward
-*   RPM_order -> Desired Speed (RPM)
-*   PWM_order -> I(k+1)
-*   PWM_order_last -> I(k)
-*   Error -> Erreur(k+1)
-*   Error_last -> Erreur(k)
-*/
-void recurrence_equation(double RPM_order, double Error, double& Error_last, double& PWM_order, double& PWM_order_last, double currentSpeed);  
-// int *
+// Calculate the recurrence equation based on the compensator to move the car forward and backward
+void recurrence_equation(bool attenuation, double RPM_order, double Error, double& Error_last, double& PWM_order, double& PWM_order_last, double currentSpeed);  
+
 void compensator_recurrence(bool init, double RPM_order, bool reverse, double currentRightSpeed, double currentLeftSpeed, uint8_t& rightRearPwmCmd, uint8_t& leftRearPwmCmd);
 
 #endif /*__ control_loop_H */
