701fcd1424951e16fb73be6bed08e3da6198b24d
ErAzZeD <kilou28800@gmail.com>
2024-01-18 10:27:17 +0100

    Adding first version of Android application

---
+++ Application/MyApplication/app/src/main/java/com/example/myapplication/GeicarData.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+class GeicarData {
+
+    companion object {
+
+        val carData = mutableMapOf<String, String>("us_front_left" to "0",
+                               "us_front_center" to "0",
+                               "us_front_right" to "0",
+                                "us_back_left" to "0",
+                                "us_back_center" to "0",
+                                "us_back_right" to "0",
+                                "trailer_angle" to "0",
+                                "speed" to "0")
+
+
+    }
+
+
+
+
+
+}
