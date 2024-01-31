11222b741896eb5f91f2303f51adb8f248015c5f
Fraumar Emilie <fraumar@insa-20425>
2024-01-31 14:07:42 +0100

    Final version

--- raspberryPI3/ros2_ws/src/obs_detect/obs_detect/obs_detection.py
+++ raspberryPI3/ros2_ws/src/obs_detect/obs_detect/obs_detection.py
@@ -22,17 +22,27 @@ class ObsDetection(Node):
         self.subscription_us = self.create_subscription(Ultrasonic, 'us_data', self.us_callback, 10)
         self.subscription_appli_order = self.create_subscription(AppliOrder, 'appli_order', self.appliOrder_callback, 10)
 
+        # Global variable
         self.sensors = False
 
+    """ Update appli information from receive_data [callback function]  :
+        This function is called when a message is published on the "/appli_order" topic
+    """
     def appliOrder_callback(self, appliOrder: AppliOrder): 
         self.sensors = appliOrder.button_sensors; 
 
+    """ Update us sensors from us_data [callback function]  :
+        This function is called when a message is published on the "/us_data" topic 
+        and publish the presence of an obstacle on the "/obstacle_detection" topic
+    """
     def us_callback(self, us: Ultrasonic):
 
         obstacle = ObstacleDetection()
 
+        # Sensors activates
         if (self.sensors):
-            # Get obstacle position
+
+            # Get obstacle near the car
             if us.front_left < self.MINIMAL_DISTANCE :
                 obstacle.obstacle_detected_front_left = True
             elif us.front_center < self.MINIMAL_DISTANCE :
@@ -55,6 +65,7 @@ class ObsDetection(Node):
                 obstacle.obstacle_detected_rear_center = False
                 obstacle.obstacle_detected_rear_right = False
 
+            # Global front and rear obstacle detection
             if obstacle.obstacle_detected_rear_left or obstacle.obstacle_detected_rear_center or obstacle.obstacle_detected_rear_right :
                 obstacle.obstacle_detected_rear = True
             else :
@@ -64,10 +75,13 @@ class ObsDetection(Node):
                 obstacle.obstacle_detected_front = True
             else :
                 obstacle.obstacle_detected_front = False
+        
+        # Sensors deactivates
         else:
             obstacle.obstacle_detected_rear = False
             obstacle.obstacle_detected_front = False         
 
+        # Publish the presence of an obstacle on the "/obstacle_detection" topic
         self.publish_obstacle_detection.publish(obstacle)
 
 
